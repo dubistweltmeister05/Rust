2661651cf3514d4a847541a65a7c6aabc4a89485
kshitij <kshitij.vaze@tor.ai>
2025-09-09 15:14:08 +0530

    .

--- references_and_borrowing/src/main.rs
+++ references_and_borrowing/src/main.rs
@@ -2,10 +2,10 @@ fn main(){
     let m1=String::from("Hello!");
     let m2=String::from("Hello!");
     greet(&m1, &m2);
-    let s = format!("{}, {}",m1,m2);
+    let _s = format!("{}, {}",m1,m2);
 }
 
-fn greet (S1:&str, S2:&str)  {
-    println!("{}, {}",S1, S2);
+fn greet (s1:&str, s2:&str)  {
+    println!("{}, {}",s1, s2);
 
 }
